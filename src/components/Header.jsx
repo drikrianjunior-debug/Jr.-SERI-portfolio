e5dd1ff1e15626a8b4fb03ce26b7bd96f38b7a59
Drikrian Junior <drikrianjunior@gmail.com>
2026-05-28 18:06:13 +0000

    Ajout et mise à jour de page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ export default function Header() {
   return (
     <header style={{
       position: 'absolute',
-      top: '2.5rem', left: '0.45rem', right: '3rem',
+      top: '2.5rem', left: '0.6rem', right: '3rem',
       display: 'flex', justifyContent: 'space-between', alignItems: 'center',
       pointerEvents: 'auto'
     }}>
-      <div  className="cinzel-title" style={{ fontSize: '1.4rem', color : 'white', fontWeight: '540', letterSpacing: '-0.5px' }}>
+      <div  className="cinzel-title" style={{ fontSize: '1.3rem', color : 'white', fontWeight: '540', letterSpacing: '-0.5px' }}>
         JR. SERI
       </div>
 
